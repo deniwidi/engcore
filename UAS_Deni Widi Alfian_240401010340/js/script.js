fa4f38510ccd44e8b48d4607b1b658ce50da96dc
deniwidi <deniwidialfian@gmail.com>
2025-07-27 11:30:50 +0300

    Delete UAS_Deni Widi Alfian_240401010340 directory

--- UAS_Deni Widi Alfian_240401010340/js/script.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementById("navbar");
-
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 0) {
-        navbar.classList.add("shadow");
-    } else {
-        navbar.classList.remove("shadow");
-    }
-});
